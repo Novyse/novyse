14a63410ff160ab6ae2997cc07b40b9fcc607878
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2025-09-23 18:18:23 +0200

    Refactor useEffect in useAppInit hook to properly handle async initialization

--- app/hooks/useAppInit.js
+++ app/hooks/useAppInit.js
@@ -6,12 +6,15 @@ import SocketMethods from "../utils/backend-services/socket-io";
 const useAppInit = (shouldBeLoggedIn) => {
   const router = useRouter();
 
-  useEffect(async () => {
-    const success = await auth.checkShouldBeHere(router, shouldBeLoggedIn);
-    // User should be logged in and IS logged in, then initialize app
-    if (shouldBeLoggedIn & success) {
-      await SocketMethods.openSocketConnection();
-    }
+  useEffect(() => {
+    const appInit = async () => {
+      const success = await auth.checkShouldBeHere(router, shouldBeLoggedIn);
+      // User should be logged in and IS logged in, then initialize app
+      if (shouldBeLoggedIn & success) {
+        await SocketMethods.openSocketConnection();
+      }
+    };
+    appInit();
   }, []);
 };
 
