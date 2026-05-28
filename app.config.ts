fe115e18d1fff2691953540473f4b3aba2bf7fd4
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2026-05-28 14:58:39 +0200

    Change BRANCH constant from 'preview' to 'development'

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ const APP_SLUG = "novyse";
 const APP_VERSION = packageJson.version;
 const EXPO_OWNER = "novyse";
 const EAS_PROJECT_ID = "3f91b058-96c7-45ff-abb5-511b5d084b64";
-const BRANCH = "preview" as "development" | "preview" | "production";
+const BRANCH = "development" as "development" | "preview" | "production";
 
 const getDomain = (sub: string) => {
   const suffix =
