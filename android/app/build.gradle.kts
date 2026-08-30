900652c143e5fa32adccf51e7c1e7e9bfef5c224
SamueleOrazioDurante <samueleorazio.durante@drnt.dev>
2026-08-30 23:32:37 +0200

    chore: upgrade compileSdk to 37 and update auth-sdk package dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.novyse.novyse"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
