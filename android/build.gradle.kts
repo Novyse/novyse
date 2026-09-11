3cc329b350208ef9f4dc386fb23b7ba189b2a705
SamueleOrazioDurante <samueleorazio.durante@drnt.dev>
2026-09-11 20:30:00 +0200

    chore: replace legacy build file patching with Gradle extension configuration for Java 17 (this forces java 17, needed for flutter_webrtc)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,33 +14,41 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-
-    // Automatically patch legacy plugins for AGP 9
-    val bf = project.buildFile
-    if (bf.exists()) {
-        try {
-            var content = bf.readText()
-            var modified = false
-            if (content.contains("proguard-android.txt")) {
-                content = content.replace("proguard-android.txt", "proguard-android-optimize.txt")
-                modified = true
-            }
-            if (content.contains("agpMajor < 9")) {
-                content = content.replace("agpMajor < 9", "true")
-                modified = true
-            }
-            if (content.contains("JavaVersion.VERSION_1_8")) {
-                content = content.replace("JavaVersion.VERSION_1_8", "JavaVersion.VERSION_17")
-                modified = true
-            }
-            if (modified) {
-                bf.writeText(content)
-            }
-        } catch (_: Exception) {}
-    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    fun forceJava17() {
+        try {
+            @Suppress("DEPRECATION")
+            extensions.configure<com.android.build.gradle.LibraryExtension> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        } catch (_: Exception) {}
+        try {
+            extensions.configure<com.android.build.api.dsl.LibraryExtension> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        } catch (_: Exception) {}
+    }
+    pluginManager.withPlugin("com.android.library") {
+        forceJava17()
+    }
+    if (project.path != ":app") {
+        project.afterEvaluate {
+            try { forceJava17() } catch (_: Exception) {}
+        }
+    }
+    tasks.configureEach {
+        if (this is org.jetbrains.kotlin.gradle.tasks.KotlinCompile) {
+            compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
